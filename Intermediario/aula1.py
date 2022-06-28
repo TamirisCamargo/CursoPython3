cab3bd21d8723c2a1d7f75ec91f450aa3f1b945a
EngageTamiris <87433670+TamirisCamargo@users.noreply.github.com>
2022-06-27 22:59:49 -0300

    new classes

---
+++ Intermediario/aula1.py
@@ -0,0 +1,21 @@
+"""
+Funções - def em Python (Parte 1)
+"""
+
+def funcao():
+    print('Hello World')
+
+funcao()
+funcao()
+funcao()
+
+def saudacao(msg='Olá', nome='usuário'):
+    nome = nome.replace('e', '3')
+    msg = msg.replace('e', '3')
+    print(msg, nome)
+
+saudacao(nome='Tami', msg='Hi')
+saudacao('Oi', 'Luiz')
+saudacao('Hello', 'Maria')
+saudacao('Olá', 'Otávio')
+saudacao('Olá', 'João')
