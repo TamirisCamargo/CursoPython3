e5372295ea709770da52437d449f2f1e822b2c4b
EngageTamiris <87433670+TamirisCamargo@users.noreply.github.com>
2022-07-25 22:06:26 -0300

    new classes

---
+++ Intermediario/teste.py
@@ -0,0 +1,13 @@
+def myMethod(param1, param2, param3):
+    print(param1, param2, param3)
+
+
+myMethod("String", 1, 10)
+type(myMethod("String", 1, 10))
+
+
+def soma(param2, param3):
+    print(param2 + param3)
+
+
+soma(param2=1, param3=10)
